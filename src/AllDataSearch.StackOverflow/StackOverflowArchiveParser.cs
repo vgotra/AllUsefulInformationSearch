58ab5bb36c282f3ed1468401005c75e0d21d219d
Volodymyr Gotra <vgotra@gmail.com>
2023-10-17 14:18:21 +0300

    Added base StackOverflow data files parser, etc

---
+++ src/AllDataSearch.StackOverflow/StackOverflowArchiveParser.cs
@@ -0,0 +1,53 @@
+using System.Text.RegularExpressions;
+
+namespace AllDataSearch.StackOverflow;
+
+public interface IStackOverflowArchiveParser
+{
+    Task<List<StackOverflowDataFile>> GetDataFileInfoListAsync();
+}
+
+public class StackOverflowArchiveParser : IStackOverflowArchiveParser
+{
+    private const string StackOverflowArchiveUrl = "https://archive.org/details/stackexchange";
+    private static readonly Regex ItemsRegex = new(
+        """<tr>\s*<td>\s*<a.*?href="(?<Link>[^<]*?7z[^<]*?)">(?<Name>[^<]*?7z[^<]*?)<\/a>.*?<\/td>\s*<td>(?<LastModified>.*?)<\/td>\s*<td>(?<Size>.*?)<\/td>\s*<\/tr>""",
+        RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public StackOverflowArchiveParser(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<List<StackOverflowDataFile>> GetDataFileInfoListAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        var archiveHtmlPage = await httpClient.GetStringAsync(StackOverflowArchiveUrl);
+        var result = ParseLines(archiveHtmlPage);
+        return result;
+    }
+
+    private List<StackOverflowDataFile> ParseLines(string htmlText)
+    {
+        var result = new List<StackOverflowDataFile>();
+        var matches = ItemsRegex.Matches(htmlText);
+        foreach (Match match in matches)
+        {
+            var item = ParseLine(match);
+            result.Add(item);
+        }
+        return result;
+    }
+
+    private StackOverflowDataFile ParseLine(Match match)
+    {
+        var link = match.Groups["Link"].Value;
+        var name = match.Groups["Name"].Value;
+        var lastModified = DateTime.Parse(match.Groups["LastModified"].Value);
+        var size = match.Groups["Size"].Value;
+
+        return new StackOverflowDataFile { Name = name, Link = link, Size = size, LastModified = lastModified };
+    }
+}
---
+++ src/AllDataSearch.StackOverflow/StackOverflowDataFile.cs
@@ -0,0 +1,9 @@
+﻿namespace AllDataSearch.StackOverflow;
+
+public record StackOverflowDataFile
+{
+    public string Name { get; set; } = null!;
+    public string Link { get; set; } = null!;
+    public string Size { get; set; } = null!;
+    public DateTime LastModified { get; set; }
+}
