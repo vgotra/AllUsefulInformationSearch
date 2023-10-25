6c7178dfa201616c0966204daf6c20a0e315afd9
Volodymyr Gotra <vgotra@gmail.com>
2023-10-25 21:16:02 +0300

    Updated tests and some packages, etc.

--- src/AllUsefulInformationSearch.DatabaseMigrations/Migrations/Migration1000.cs
+++ src/AllUsefulInformationSearch.DatabaseMigrations/Migrations/Migration1000.cs
@@ -5,7 +5,7 @@ public class Migration1000 : MigrationBase
 {
     public override void Up()
     {
-        Db.CreateCommand().ExecNonQuery("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"); // Enable Guid auto generation
+        Db!.CreateCommand().ExecNonQuery("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"); // Enable Guid auto generation
         Db.CreateTable<InformationSourceDataFile>();
     }
 
