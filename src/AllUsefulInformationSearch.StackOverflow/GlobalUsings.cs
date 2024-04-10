c61b252fa593055c2e017e01da3c10408a9c9c2a
Volodymyr Gotra <vgotra@gmail.com>
2024-04-10 14:51:39 +0300

    Updated source code

--- src/AllUsefulInformationSearch.StackOverflow/GlobalUsings.cs
+++ src/AllUsefulInformationSearch.StackOverflow/GlobalUsings.cs
@@ -1,8 +1,3 @@
-﻿// Global using directives
-
-global using AllUsefulInformationSearch.Common.Http;
-global using AllUsefulInformationSearch.StackOverflow.Extensions;
-
+﻿global using AllUsefulInformationSearch.StackOverflow.Extensions;
 global using Flurl.Http;
-
 global using System.Text.RegularExpressions;
