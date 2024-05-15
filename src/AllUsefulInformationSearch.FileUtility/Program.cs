d9aef7867624a1273a21e00c356d06bee92ee918
Volodymyr Gotra <vgotra@gmail.com>
2024-05-15 14:46:56 +0300

    Updated source code

--- src/AllUsefulInformationSearch.FileUtility/Program.cs
+++ src/AllUsefulInformationSearch.FileUtility/Program.cs
@@ -21,12 +21,41 @@ static class Program
         httpClient.DefaultRequestHeaders.Range = new RangeHeaderValue(0, 0);
         
         var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, fileUrl), cancellationToken);
-        var contentRange = response.Content.Headers.ContentRange; //TODO Check content range 
+        var contentRange = response.Content.Headers.ContentRange;
+        var fileSize = (int)(contentRange?.Length ?? -1);
+        if (fileSize is -1 or < 1024 * 1024)
+        {
+            await using var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.Write, 4096, true);
+            var webStream = await httpClient.GetStreamAsync(fileUrl, cancellationToken);
+            await webStream.CopyToAsync(fileStream, cancellationToken);
+            return;
+        }
 
-        await using var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
-        var webStream = await httpClient.GetStreamAsync(fileUrl, cancellationToken);
-        await webStream.CopyToAsync(fileStream, cancellationToken);
-        
-        //TODO Add support for downloading in chunks for faster download
+        await using var chunksFileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.Write, 4096, true);
+        var chunks = SplitFileIntoChunks(fileSize, 1024 * 1024);
+        await Parallel.ForEachAsync(chunks, cancellationToken, async (range, token) =>
+        {
+            //TODO Check this later
+            using var chunkHttpClient = new HttpClient(); //TODO Check for thread safety
+            chunkHttpClient.DefaultRequestHeaders.Range = new RangeHeaderValue(range.From, range.To);
+            var chunkResponse = await httpClient.GetAsync(fileUrl, token);
+            var chunkBytes = await chunkResponse.Content.ReadAsByteArrayAsync(token);
+            var offset = range.From == 0 ? 0 : range.From;
+            await chunksFileStream.WriteAsync(chunkBytes, offset, chunkBytes.Length, token);
+        });
+    }
+    
+    private static List<(int From, int To)> SplitFileIntoChunks(long fileSize, int chunkSize)
+    {
+        var chunks = new List<(int From, int To)>();
+        for (var i = 0; i < fileSize; i += chunkSize)
+        {
+            var from = i;
+            var to = i + chunkSize - 1;
+            if (to > fileSize - 1)
+                to = (int)(fileSize - 1);
+            chunks.Add((from, to));
+        }
+        return chunks;
     }
 }
