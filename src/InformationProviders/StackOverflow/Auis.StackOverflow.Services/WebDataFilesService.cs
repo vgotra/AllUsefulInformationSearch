93f53d3fdb1a7b6f13d3b79cb4547e07eb21056f
Volodymyr Gotra <vgotra@gmail.com>
2024-06-08 01:37:09 +0300

    Updated files to skip

--- src/InformationProviders/StackOverflow/Auis.StackOverflow.Services/WebDataFilesService.cs
+++ src/InformationProviders/StackOverflow/Auis.StackOverflow.Services/WebDataFilesService.cs
@@ -2,12 +2,12 @@
 
 public class WebDataFilesService(StackOverflowDbContext dbContext, IWebArchiveParserService parserService, ILogger<WebDataFilesService> logger) : IWebDataFilesService
 {
-    private readonly string[] _fileNamesToSkip = ["stackoverflow.com-Badges.7z"]; //TODO Move this later to configuration/settings
-    
+    private readonly string[] _fileNamesToSkip = ["stackoverflow.com-Badges.7z", "stackoverflow.com-PostLinks.7z", "stackoverflow.com-Tags.7z", "stackoverflow.com-Users.7z", "stackoverflow.com-Comments.7z", "stackoverflow.com-Votes.7z", "stackoverflow.com-PostHistory.7z"]; //TODO Move this later to configuration/settings
+
     public async Task SynchronizeWebDataFilesAsync(CancellationToken cancellationToken = default)
     {
         logger.LogInformation("Started syncing web data files to database");
-        
+
         var archiveFiles = await parserService.GetFileInfoListAsync(cancellationToken);
         var archiveFileDict = archiveFiles.ToDictionary(x => x.Name, x => x);
         var dataFiles = await dbContext.WebDataFiles.ToListAsync(cancellationToken);
@@ -23,7 +23,7 @@ public class WebDataFilesService(StackOverflowDbContext dbContext, IWebArchivePa
         await dbContext.AddRangeAsync(webFiles, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
         // await dbContext.BulkInsertOrUpdateAsync(webFiles, cancellationToken: cancellationToken);
-        
+
         logger.LogInformation("Synchronized web data files to database");
     }
 }
