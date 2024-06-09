c5cbd458665a610b63ae5c36593ce0233dad7173
Volodymyr Gotra <vgotra@gmail.com>
2024-06-09 19:05:59 +0300

    Added text cleanup

--- src/InformationProviders/StackOverflow/Auis.StackOverflow.Services/PostModificationService.cs
+++ src/InformationProviders/StackOverflow/Auis.StackOverflow.Services/PostModificationService.cs
@@ -1,13 +1,33 @@
-﻿namespace Auis.StackOverflow.Services;
+﻿using System.Text.RegularExpressions;
+
+namespace Auis.StackOverflow.Services;
 
 public class PostModificationService(ILogger<PostModificationService> logger) : IPostModificationService
 {
+    private static readonly Regex LiRegex = new(@"<li>", RegexOptions.Compiled);
+    private static readonly Regex AllHtmlExceptLinks = new("<(?!a|/a).*?>", RegexOptions.Compiled);
+    private static readonly Regex MultipleLineEnds = new("\n{2,}", RegexOptions.Compiled);
+    private static readonly Regex MultipleSpaces = new(" {2,}", RegexOptions.Compiled);
+
     public async Task<List<PostModel>> PostProcessArchivePostsAsync(List<PostModel> posts, CancellationToken cancellationToken = default)
     {
         logger.LogInformation("Starting post processing of archive posts");
-        await Task.CompletedTask;
-        //TODO Apply cleanup if needed, filtering, etc
+        foreach (var post in posts)
+        {
+            post.Body = CleanupText(post.Body);
+            post.AcceptedAnswer!.Body = CleanupText(post.AcceptedAnswer.Body);
+        }
         logger.LogInformation("Completed post processing of archive posts");
+        await Task.CompletedTask;
         return posts;
+    }
+
+    private string CleanupText(string text)
+    {
+        var cleanedText = LiRegex.Replace(text, "- ");
+        cleanedText = AllHtmlExceptLinks.Replace(cleanedText, string.Empty);
+        cleanedText = MultipleLineEnds.Replace(cleanedText, "\n");
+        cleanedText = MultipleSpaces.Replace(cleanedText, "\n");
+        return cleanedText;
     }
 }
